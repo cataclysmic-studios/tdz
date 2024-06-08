e931b597ec4041e851ac32295bd90a64b3fbd1a2
R-unic <roleyspersonal@gmail.com>
2024-06-08 19:06:32 -0400

    feat: create LoadScreen & Spin components

--- types/PlayerGui.d.ts
+++ types/PlayerGui.d.ts
@@ -1,3 +1,15 @@
 interface PlayerGui extends BasePlayerGui {
-
+  LoadScreen: ScreenGui & {
+    Background: ImageLabel & {
+      UIGradient: UIGradient;
+      UIPadding: UIPadding;
+      Spinner: ImageLabel & {
+        UIAspectRatioConstraint: UIAspectRatioConstraint;
+        MiniSpinner: ImageLabel;
+      };
+      Logo: ImageLabel & {
+        UIAspectRatioConstraint: UIAspectRatioConstraint;
+      };
+    };
+  };
 }
