67831e9538ad7bec6848ca773b4021ccf17bbfa4
R-unic <roleyspersonal@gmail.com>
2024-06-28 00:28:36 -0400

    fix: summon-enemy command checks if enemy exists before calling EnemyService#summon()

--- places/main/src/server/cmdr/commands/summon-enemiesServer.ts
+++ places/main/src/server/cmdr/commands/summon-enemiesServer.ts
@@ -1,14 +1,21 @@
 import { Dependency } from "@flamework/core";
 import type { CommandContext } from "@rbxts/cmdr";
 
+import { Assets } from "common/shared/utility/instances";
+
 import type { EnemyService } from "server/services/enemy";
 
 export = function (context: CommandContext, name: EnemyName, amount: number, interval: number): void {
   const enemy = Dependency<EnemyService>();
   try {
-    enemy.summon({
-      enemyName: name,
-      amount, interval
+    if (Assets.Enemies[name] === undefined)
+      return context.Reply(`Error: "${name}:" is not a valid enemy name.`, Color3.fromRGB(255, 0, 0));
+
+    task.spawn(() => {
+      enemy.summon({
+        enemyName: name,
+        amount, interval
+      });
     });
     context.Reply(`Successfully spawned ${amount} ${name}(s)!`, Color3.fromRGB(0, 255, 0));
   } catch (err) {
