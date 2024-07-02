e7be384b63eee68f36d2b120c96932c221a75cea
R-unic <roleyspersonal@gmail.com>
2024-07-02 19:14:27 -0400

    feat: add waves 5 and 6

--- places/main/src/shared/waves/easy.ts
+++ places/main/src/shared/waves/easy.ts
@@ -3,7 +3,7 @@ import type { WaveData } from "shared/structs";
 // the length of the last wave doesnt matter so i am setting it to -1 as a marker.
 
 const EASY_WAVES: WaveData[] = [
-  {
+  { // 1
     length: "1 minute",
     completionReward: 200,
     enemies: [
@@ -14,7 +14,7 @@ const EASY_WAVES: WaveData[] = [
         length: -1
       }
     ]
-  }, {
+  }, { // 2
     length: "1 minute",
     completionReward: 280,
     enemies: [
@@ -25,7 +25,7 @@ const EASY_WAVES: WaveData[] = [
         length: -1
       }
     ]
-  }, {
+  }, { // 3
     length: "1 minute",
     completionReward: 360,
     enemies: [
@@ -41,7 +41,7 @@ const EASY_WAVES: WaveData[] = [
         length: -1
       }
     ]
-  }, {
+  }, { // 4
     length: "1 minute",
     completionReward: 360,
     enemies: [
@@ -58,6 +58,43 @@ const EASY_WAVES: WaveData[] = [
       }, {
         enemyName: "Zombie",
         amount: 4,
+        interval: 1,
+        length: -1
+      }
+    ]
+  }, { // 5
+    length: "1 minute",
+    completionReward: 360,
+    enemies: [
+      {
+        enemyName: "Fast Zombie",
+        amount: 8,
+        interval: 1,
+        length: 2
+      }, {
+        enemyName: "Heavy Zombie",
+        amount: 10,
+        interval: 1.2,
+        length: -1
+      }
+    ]
+  }, { // 6
+    length: "1 minute",
+    completionReward: 400,
+    enemies: [
+      {
+        enemyName: "Zombie",
+        amount: 8,
+        interval: 1,
+        length: 3
+      }, {
+        enemyName: "Heavy Zombie",
+        amount: 8,
+        interval: 1.2,
+        length: 5
+      }, {
+        enemyName: "Stealth Zombie",
+        amount: 1,
         interval: 1,
         length: -1
       }
