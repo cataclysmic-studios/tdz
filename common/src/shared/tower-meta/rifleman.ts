5f5c11b960ecaced2a03e7277121ffbf2bf73919
CollinsVenture <zillinod@gmail.com>
2024-07-03 22:42:33 -0400

    Update rifleman.ts

--- common/src/shared/tower-meta/rifleman.ts
+++ common/src/shared/tower-meta/rifleman.ts
@@ -4,36 +4,36 @@ const RiflemanMeta = identity<TowerMeta[][]>([
   [ // path 1
     {
       name: "Focus",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325395028"
     }, {
       name: "Speed Training",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325403805"
     }, {
       name: "Thompson SMG",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325415475"
     }, {
       name: "Back Support",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325430042"
     }, {
       name: "M1918 BAR",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325442872"
     }
   ], [ // path 2
     {
       name: "Enhanced Ballistics",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325457212"
     }, {
       name: "Enhanced Optics",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325466881"
     }, {
       name: "M1903 Springfield",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325476876"
     }, {
       name: "High Caliber",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325457212"
     }, {
       name: "M1941 Johnson Rifle",
-      icon: "rbxassetid://10909681909"
+      icon: "rbxassetid://18325492061"
     }
   ]
 ]);
